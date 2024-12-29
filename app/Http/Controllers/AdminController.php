274c18fbd19e68e8c58f6d645339e9c0b0144e25
Deepanker Tiwari <deepamamazing@gmail.com>
2024-12-29 21:57:32 +0530

    Launch Pad

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function view_category()
+    {
+        $data = Category::all();
+        return view('admin.category', compact('data'));
+    }
+
+    public function add_category(Request $request)
+    {
+        $category = new Category;
+
+        $category->category_name = $request->category;        //category table
+
+        $category->save();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category Added Successfully');
+
+        return redirect()->back();
+    }
+
+    public function delete_category($id)
+    {
+        $data = Category::find($id);
+        $data->delete();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category Deleted Successfully');
+
+
+        return redirect()->back();
+    }
+
+    public function edit_category($id)
+    {
+        $data = Category::find($id);
+
+        return view('admin.edit_category', compact('data'));
+    }
+
+    public function update_category(Request $request, $id)
+    {
+        $data = Category::find($id);
+
+        $data->category_name = $request->category;
+
+        $data->save();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Category Updated Successfully');
+
+
+        return redirect('/view_category');
+        // return redirect('view_category');
+    }
+
+    public function add_product()
+    {
+
+        $category = Category::all();
+        return view('admin.add_product', compact('category'));
+    }
+
+    public function upload_product(Request $request)
+    {
+        $data = new Product;
+        $data->title = $request->title;
+        $data->description = $request->description;
+        $data->price = $request->price;
+        $data->quantity = $request->qty;
+        $data->category = $request->category;
+
+        $image = $request->image;
+        if ($image) {
+            $imagename = time() . '.' . $image->getClientOriginalExtension();
+            $request->image->move('products', $imagename);
+            $data->image = $imagename;
+        }
+
+        $data->save();
+
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Product Added Successfully');
+
+
+        return redirect()->back();
+    }
+
+    public function view_product()
+    {
+
+        $product = Product::paginate(5);
+        return view('admin.view_product', compact('product'));
+    }
+
+    public function delete_product($id)
+    {
+        $data = Product::find($id);
+
+        $image_path = public_path('products/' . $data->image);
+
+        if (file_exists($image_path)) {
+            unlink($image_path);
+        }
+
+        $data->delete();
+        toastr()->timeOut(10000)->closeButton()->addSuccess('Product Deleted Successfully');
+
+
+        return redirect()->back();
+    }
+}
---
+++ resources/views/admin/view_product.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    @include('admin.css')
+
+
+    <style type="text/css">
+        .div_deg {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 60px;
+        }
+
+        .table_deg {
+            border: 2px solid greenyellow;
+        }
+
+        th {
+            background-color: skyblue;
+            color: white;
+            font-size: 19px;
+            padding: 15px;
+            font-weight: bold;
+        }
+
+        td {
+            border: 1px solid skyblue;
+            text-align: center;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+    {{-- Header Section  --}}
+
+    @include('admin.header')
+
+
+    {{-- Sidebar Section --}}
+
+    @include('admin.sidebar')
+    <!-- Sidebar Navigation end-->
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
+
+                <div class="div_deg">
+                    <table class="table_deg">
+                        <tr>
+                            <th>Product Title</th>
+                            <th>Description</th>
+                            <th>Category</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Image</th>
+                            <th>Delete</th>
+                        </tr>
+
+                        @foreach ($product as $products)
+                            <tr>
+                                <td>{{ $products->title }}</td>
+                                <td>{!! Str::limit($products->description, 10) !!}</td>
+                                {{-- Limited Text --}}
+                                <td>{{ $products->category }}</td>
+                                <td>{{ $products->price }}</td>
+                                <td>{{ $products->quantity }}</td>
+                                <td><img height="120" width="120" src="products/{{ $products->image }}"></td>
+                                <td><a class="btn btn-danger" onclick="confirmation(event)"
+                                        href="{{ url('delete_product', $products->id) }}">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+
+
+                    </table>
+
+                </div>
+
+                <div class="div_deg">
+                    {{ $product->onEachSide(1)->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- JavaScript files-->
+    @include('admin.js');
+
+</body>
+
+</html>
