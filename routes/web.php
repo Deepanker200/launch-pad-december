682975b6a57f01b8bd33dca63c8d6f4d8d32452f
Deepanker Tiwari <deepamamazing@gmail.com>
2025-01-30 11:47:56 +0530

    Displaying products on home page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [HomeController::class, 'login_home'])->
+    middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -66,3 +66,6 @@ Route::post('edit_product/{id}', [AdminController::class, 'edit_product'])
 
 Route::get('product_search', [AdminController::class, 'product_search'])
     ->middleware(['auth', 'admin']);
+
+
+Route::get('product_details/{id}',[HomeController::class,'product_details']);
