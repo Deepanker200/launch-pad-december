7d23a466c7c2f54694cbd467bf38b0a310e583af
Deepanker Tiwari <deepamamazing@gmail.com>
2025-02-23 11:58:32 +0530

    Admin css changes

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -6,8 +6,8 @@
             <div class="avatar"><img src="{{ asset('admincss/img/avatar-6.jpg') }}" alt="..."
                     class="img-fluid rounded-circle"></div>
             <div class="title">
-                <h1 class="h5">Mark Stephen</h1>
-                <p>Web Designer</p>
+                <h1 class="h5">Deepanker Tiwari</h1>
+                <p>Software Developer</p>
             </div>
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
